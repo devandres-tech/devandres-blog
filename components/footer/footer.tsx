ab17b45b8fcde822005cadb1e378b5c4c0510f00
andres alcocer <andresalcocer7@yahoo.com>
2022-02-14 16:39:26 -0500

    set social link handles

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,20 @@ export default function Footer() {
   return (
     <footer>
       <div className={styles.footerContainer}>
-        <p>2022, built by Andres Alcocer and powered by Next.js</p>
+        <p>
+          <span>&#169;</span> 2022, built by Andres Alcocer and powered by{' '}
+          <a className={styles.linkTag} href={'https://nextjs.org/'}>
+            <b>Next.js</b>
+          </a>
+        </p>
+        <p className={styles.sourceText}>
+          <a
+            className={styles.linkTag}
+            href='https://github.com/devandres-tech/devandres-blog'
+          >
+            <b>Source code</b>
+          </a>
+        </p>
       </div>
     </footer>
   )
